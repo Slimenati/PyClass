062d671370042b52bb78b19be0ef0fa00374e1e9
Slimenati <69724805+Slimenati@users.noreply.github.com>
2022-03-16 22:05:15 -0700

    Update circle.py

--- Python class 8-22-21/circle.py
+++ Python class 8-22-21/circle.py
@@ -1,5 +1,6 @@
 from turtle import *
 
+#first circle
 penup()
 backward(200)
 
@@ -7,17 +8,9 @@ forward(50)
 pendown()
 circle(50)
 
-penup()
-forward(100)
-pendown()
-circle(50)
-
-penup()
-forward(100)
-pendown()
-circle(50)
-
-penup()
-forward(100)
-pendown()
-circle(50)
+#3 other circles
+for i in range(3):
+    penup()
+    forward(100)
+    pendown()
+    circle(50)
