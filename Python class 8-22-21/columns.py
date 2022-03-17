304c2d7373950e082b66cb2fb39c379b9c70b879
Slimenati <69724805+Slimenati@users.noreply.github.com>
2022-03-16 22:09:13 -0700

    Update columns.py

--- Python class 8-22-21/columns.py
+++ Python class 8-22-21/columns.py
@@ -1,17 +1,22 @@
 from turtle import *
 
+#first column
 left(90)
 forward(200)
 backward(400)
 penup()
 left(90)
 forward(100)
+
+#second column
 right(90)
 pendown()
 forward(400)
 penup()
 right(90)
 forward(200)
+
+#third column
 pendown()
 right(90)
 forward(400)
