dc8102418f468ea1f7158f42e0f21fdb98bcf13b
Slimenati <69724805+Slimenati@users.noreply.github.com>
2022-03-20 12:13:21 -0700

    Add files via upload

---
+++ Python class 8-29-21/drawing.py
@@ -0,0 +1,71 @@
+#Utlizing circles and loops, this program creates a somewhat spiral shape of 
+#all the different colors.
+speed(0)
+for i in range (70):  
+    color("black")
+    right(0.2)
+    circle(282.842712475)
+for i in range (70):  
+    color("blue")
+    right(0.2)
+    circle(282.842712475)
+for i in range (70):  
+    color("light blue")
+    right(0.2)
+    circle(282.842712475)
+for i in range (70):  
+    color("brown")
+    right(0.2)
+    circle(282.842712475)
+for i in range (70):  
+    color("cyan")
+    right(0.2)
+    circle(282.842712475)
+for i in range (70):  
+    color("gold")
+    right(0.2)
+    circle(282.842712475)
+for i in range (70):  
+    color("gray")
+    right(0.2)
+    circle(282.842712475)
+for i in range (70):  
+    color("green")
+    right(0.2)
+    circle(282.842712475)
+for i in range (70):  
+    color("light green")
+    right(0.2)
+    circle(282.842712475)
+for i in range (70):  
+    color("indigo")
+    right(0.2)
+    circle(282.842712475)
+for i in range (70):  
+    color("orange")
+    right(0.2)
+    circle(282.842712475)
+for i in range (70):  
+    color("pink")
+    right(0.2)
+    circle(282.842712475)
+for i in range (70):  
+    color("purple")
+    right(0.2)
+    circle(282.842712475)
+for i in range (70):  
+    color("red")
+    right(0.2)
+    circle(282.842712475)
+for i in range (70):  
+    color("light red")
+    right(0.2)
+    circle(282.842712475)
+for i in range (70):  
+    color("violet")
+    right(0.2)
+    circle(282.842712475)
+for i in range (70):  
+    color("yellow")
+    right(0.2)
+    circle(282.842712475)
---
+++ Python class 8-29-21/hexagon.py
@@ -0,0 +1,7 @@
+begin_fill()
+color('light green')
+speed(0)
+for i in range (6):
+    forward(50)
+    left(60)
+end_fill()
