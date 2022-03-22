141a82711b08fe8e3a5e14b87dd6f6177eb0d403
Slimenati <69724805+Slimenati@users.noreply.github.com>
2022-03-21 22:12:25 -0700

    Update vote.py

--- Python class 9-26-21/vote.py
+++ Python class 9-26-21/vote.py
@@ -2,4 +2,5 @@
 age = int(input("What is your age?"))
 if age < 18:
     print("You are not old enough to vote.")
-else: print("You are old enough to vote!")
+else: 
+    print("You are old enough to vote!")
