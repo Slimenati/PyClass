145ae823b7427acbc4cc1cfef8319e31f43c603d
Slimenati <69724805+Slimenati@users.noreply.github.com>
2022-03-21 22:06:51 -0700

    Add files via upload

---
+++ Python class 9-19-21/population.py
@@ -0,0 +1,7 @@
+#finds the population of the United States and the state of New York
+USpopulation = int(332759738)
+NYstatepopulation = int(20174987)
+percentofUS = (20174987/332759738 * 100)
+print (f"The US has a population of {USpopulation}.")
+print (f"The state of New York has a population of {NYstatepopulation}.")
+print (f"The population of New York is {percentofUS}% of the US population.")
