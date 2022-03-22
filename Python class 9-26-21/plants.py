3b2628b49da153c4154218ecb4895f896ec82f2f
Slimenati <69724805+Slimenati@users.noreply.github.com>
2022-03-21 22:14:54 -0700

    Create plants.py

---
+++ Python class 9-26-21/plants.py
@@ -0,0 +1,7 @@
+#tells you if the plant needs watering or repotting
+needs_water = True
+needs_to_be_repotted = False
+if needs_water == True:
+    print (needs_water)
+if needs_to_be_repotted == False:
+    print (needs_to_be_repotted)
