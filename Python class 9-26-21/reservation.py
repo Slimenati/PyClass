09c06e9dc7557dadf79c3fa6a4fa65907d98fb29
Slimenati <69724805+Slimenati@users.noreply.github.com>
2022-03-21 22:11:58 -0700

    Update reservation.py

--- Python class 9-26-21/reservation.py
+++ Python class 9-26-21/reservation.py
@@ -3,4 +3,5 @@ name = str(input("What is your name?"))
 reservation_name = "Jason"
 if name == reservation_name:
     print("Right this way!")
-else: print("Sorry, we don't have a reservation under that name.")
+else:
+    print("Sorry, we don't have a reservation under that name.")
