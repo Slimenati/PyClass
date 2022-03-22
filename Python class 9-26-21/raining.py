d71300930c79f56f6b5f1a6116bc1bd51c840592
Slimenati <69724805+Slimenati@users.noreply.github.com>
2022-03-21 22:18:11 -0700

    Add files via upload

---
+++ Python class 9-26-21/raining.py
@@ -0,0 +1,6 @@
+#responds uniquely for either if it's raining or not
+raining = True
+if raining == True:
+    print ("I'm going to dance in the rain!")
+else:
+    print ("I'm going to dance in the sun!")
