ad843d836318942b325ef1e5cc2de3d59aeb9902
Slimenati <69724805+Slimenati@users.noreply.github.com>
2022-04-09 22:01:02 -0700

    Update plants.py

--- Python class 9-26-21/plants.py
+++ Python class 9-26-21/plants.py
@@ -1,7 +1,6 @@
 #tells you if the plant needs watering or repotting
 needs_water = True
 needs_to_be_repotted = False
-if needs_water == True:
-    print (needs_water)
-if needs_to_be_repotted == False:
-    print (needs_to_be_repotted)
+
+print (needs_water)
+print (needs_to_be_repotted)
